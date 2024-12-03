6c6d8554adaeec7c1971b43f82c26d940cdd61f0
Ravali-Maddela <maddelaravali12@gmail.com>
2024-12-03 11:19:21 -0600

    done naviagtion,bookings, contact

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log('MongoDB Connected');
-    } catch (error) {
-      console.error('MongoDB connection error:', error.message);
-      throw new Error('Database connection failed');
-    }
-  };
+  // Use environment variable or default to 'mongodb://mongo:27017/professionalmarketdb'
+  const dbURI =
+    process.env.MONGO_URI || "mongodb://mongo:27017/professionalmarketdb";
+
+  mongoose
+    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+      process.exit(1);
+    });
+};
 
 module.exports = connectDB;
